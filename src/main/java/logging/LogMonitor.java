ef67f0ec4ea07efb3068f3f238cbf0e36b8b6428
Sebastian Mok <sebekc_mok@hotmail.co.uk>
2025-03-27 22:44:16 +0000

    gets the four pieces of data from the log

--- src/main/java/logging/LogMonitor.java
+++ src/main/java/logging/LogMonitor.java
@@ -1,13 +1,14 @@
 package logging;
 
 import java.io.*;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class LogMonitor {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello, World!");
-
         try {
             File file = new File("src/main/resources/logs.log");
             System.out.println(logParser(file));
@@ -19,12 +20,27 @@ public class LogMonitor {
 
 
     public static String logParser(File file) throws IOException {
-        StringBuilder sb = new StringBuilder();
 
         Scanner scanner = new Scanner(file);
+        StringBuilder sb = new StringBuilder();
+
+//        this will store the data for all tasks, using the ID as the key value
+        HashMap<Integer, TaskDetails> allTasks = new HashMap<Integer, TaskDetails>();
+
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            sb.append(line).append("\n");
+            String[] taskDetailsArray = line.split(",");
+
+//           gets the four pieces of data from the current line in the log
+            LocalTime currentTime = LocalTime.parse(taskDetailsArray[0]);
+            String currentTaskName = taskDetailsArray[1];
+            String startOrEnd = taskDetailsArray[2].trim(); // removing the leading space from the string
+            int currentTaskID = Integer.parseInt(taskDetailsArray[3]);
+
+            System.out.println(startOrEnd);
+
+
+//            sb.append(line).append("\n");
         }
         scanner.close();
         return sb.toString();
---
+++ src/main/java/logging/TaskDetails.java
@@ -0,0 +1,33 @@
+package logging;
+
+import java.time.LocalTime;
+
+public class TaskDetails {
+
+    private String taskName;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+}
